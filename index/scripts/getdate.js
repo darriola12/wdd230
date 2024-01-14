c3d0a65e60ccf161f4c5f95603a89cc35641f397
darriola12 <diego.arriolav1@gmail.com>
2024-01-13 22:57:30 -0600

    cmabios

---
+++ index/scripts/getdate.js
@@ -0,0 +1,15 @@
+const options = 
+{
+    weekday : "long",
+    year : "numeric",
+    month: "long",
+    day: "numeric"
+}
+
+const date = document.querySelector("#currentDate");
+date.innerHTML = new Date().toLocaleDateString("en-US", options);
+
+const lastmodied = document.querySelector("#lastModified");
+lastmodied.innerHTML = `last time modifed: ${new Date(document.lastModified)}`;
+
+
