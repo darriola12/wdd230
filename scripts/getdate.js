3d41aea55defa8edfdf492d60acc8c95ff993693
darriola12 <diego.arriolav1@gmail.com>
2024-01-28 23:25:26 -0600

    camvios

--- scripts/getdate.js
+++ scripts/getdate.js
@@ -13,11 +13,4 @@ const lastmodied = document.querySelector("#lastModified");
 lastmodied.innerHTML = `last time modifed: ${new Date(document.lastModified)}`;
 
 
-const hamburBotton = document.querySelector("#navagation_options");
-const navagation = document.querySelector("#menu");
-
-hamburBotton.addEventListener("click", () => {
-    navagation.classList.toggle("open");
-    hamburBotton.classList.toggle("open");
-});
 
