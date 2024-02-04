80794d79daf1fd41a053a56ac77530e31d891978
darriola12 <diego.arriolav1@gmail.com>
2024-02-03 20:50:42 -0600

    cambios

--- chamber/scripts/slide.js
+++ chamber/scripts/slide.js
@@ -1,29 +1,19 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+const options = 
+{
+    weekday : "long",
+    year : "numeric",
+    month: "long",
+    day: "numeric"
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const date = document.querySelector("#currentDate");
+date.innerHTML = new Date().toLocaleDateString("en-US", options);
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+const lastmodied = document.querySelector("#lastModified");
+lastmodied.innerHTML = `last time modifed: ${new Date(document.lastModified)}`
+
+const day = lastModified.getDate();
+const year = lastModified.getFullyear();
+
+lastModified.innerHTML = `last time modified: ${day}/ ${year}`;
 
