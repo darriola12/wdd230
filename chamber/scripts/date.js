7b71c41c762b30c53df91159b97f6bdf1a538bbd
darriola12 <diego.arriolav1@gmail.com>
2024-02-07 21:38:00 -0600

    cambios

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -12,8 +12,4 @@ date.innerHTML = new Date().toLocaleDateString("en-US", options);
 const lastmodied = document.querySelector("#lastModified");
 lastmodied.innerHTML = `last time modifed: ${new Date(document.lastModified)}`
 
-const day = lastModified.getDate();
-const year = lastModified.getFullyear();
-
-lastModified.innerHTML = `last time modified: ${day}/ ${year}`;
 
