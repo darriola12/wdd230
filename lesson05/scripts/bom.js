6565409b92463c4f3f8e967fa38c7199c1ccc9a2
darriola12 <diego.arriolav1@gmail.com>
2024-02-08 22:34:51 -0600

    cambios

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -3,19 +3,17 @@ const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
 button.addEventListener("click", () => {
+    if (input.value == ""){alert("Cannot be blank")}
     if (input.value != "")
     {
+      const li = document.createElement("li");
+      const deleteButton = document.createElement("button");
 
-    }
-        
-    const li = document.createElement("li");
-    const deleteButton = document.createElement("button");
-
-    li.textContent = input.value;
-    deleteButton.textContent = "❌";
-    li.append(deleteButton);
-    list.append(li);
-    deleteButton.addEventListener("click", () => {
+      li.textContent = input.value;
+      deleteButton.textContent = "❌";
+      li.append(deleteButton);
+      list.append(li);
+      deleteButton.addEventListener("click", () => {
         list.remove(li);
         input.focus();
     })
@@ -23,6 +21,10 @@ button.addEventListener("click", () => {
     input.focus();
     input.value = "";
 
+
+    }
+        
+    
 }) 
 
 
