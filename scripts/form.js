6eda81f833fa1d01f1feba37c522b61e4db871ce
darriola12 <diego.arriolav1@gmail.com>
2024-02-29 23:29:54 -0600

    cambios

--- scripts/form.js
+++ scripts/form.js
@@ -1,23 +1,28 @@
-const password1 = document.querySelector("#password"); 
-const password2 = document.querySelector("#ConfirmPass"); 
+const ps1 = document.querySelector("#password");
+const ps2 = document.querySelector("#Confirmpassword");
 const message = document.querySelector("#message");
 
+ps2.addEventListener("focusout", checkSame);
 
-password2.addEventListener("focusout", checkSame); 
 
-function checkSame()
-{
-    if(password1.value !== password2.value)
-    {
-        message.textContent = "Passowrd do not match"
-        password2.value =""
-        password2.focus();
-    }
-    else 
-    {
-        message.style.display ="none"
-    }
+function checkSame() {
+	if (ps1.value !== ps2.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		ps2.style.backgroundColor = "#fff0f3";
+		ps2.value = "";
+		ps2.focus();
+	} else {
+		message.style.display = "none";
+		ps2.style.backgroundColor = "#fff";
+		ps2.style.color = "#000";
+	}
 }
+
+
+
+
+
 
 const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("r");
@@ -28,3 +33,23 @@ range.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+	
+	function handleSubmit(event) {
+		
+		event.preventDefault();
+
+		
+		let formt = event.target;
+		let formData = new FormData(formt);
+
+		
+		for (let pair of formData.entries()) {
+			console.log(pair[0] + ": " + pair[1]);
+		}
+	}
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", handleSubmit);
+});
