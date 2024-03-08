848664ee3c257fef553824e05f81a08bdf6c1673
darriola12 <diego.arriolav1@gmail.com>
2024-03-08 02:57:26 -0600

    cambios

--- chamber/scripts/company.js
+++ chamber/scripts/company.js
@@ -9,33 +9,50 @@ const companies = async () => {
 }
 
 function displayInformation(info) {
-  const container = document.querySelector(".container");
+  const container = document.querySelector(".container2");
   info.forEach(company => {
     const companySection = document.createElement("section");
+    companySection.className = "sectionDirectory";
 
     const companyNameHeading = document.createElement("h1");
+    companyNameHeading.classList = "headingDis";
     companyNameHeading.textContent = company.name;
 
    
     const phoneNumberHeading = document.createElement("h2");
     phoneNumberHeading.textContent = company.phoneNumber; 
+    phoneNumberHeading.classList = "info";
 
     const addressHeading = document.createElement("h3");
     addressHeading.textContent = company.addresses; 
+    addressHeading.classList = "info";
+
+
+    const website = document.createElement("p");
+    website.textContent = company.websiteURLs;
+    website.classList = "info";
+
 
     const companyImg = document.createElement("img");
+    companyImg.className = "imgDirectory"
     companyImg.setAttribute("src", company.image);
     companyImg.setAttribute("alt", company.name);
     companyImg.setAttribute("loading", "lazy");
-    companyImg.setAttribute("width", "340");
-    companyImg.setAttribute("height", "440");
+    companyImg.setAttribute("width", "140");
+    companyImg.setAttribute("height", "140");
+
+
+    const newA = document.createElement("a");
+    newA.href = company.websiteURLs;
+
+    companyNameHeading.appendChild(newA);
 
 
     companySection.appendChild(companyNameHeading);
-    companySection.appendChild(phoneNumberHeading); 
+    companySection.appendChild(companyImg);
     companySection.appendChild(addressHeading); 
-    companySection.appendChild(companyImg); 
-
+    companySection.appendChild(phoneNumberHeading); 
+    companySection.appendChild(website);
     container.appendChild(companySection);
   });
 }
