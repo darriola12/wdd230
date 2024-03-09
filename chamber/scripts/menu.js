0ff537725d101af059ef65afbb6a0e5b4fa99d17
darriola12 <diego.arriolav1@gmail.com>
2024-03-08 22:14:56 -0600

    cambios

---
+++ chamber/scripts/menu.js
@@ -0,0 +1,17 @@
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("#container2")
+
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
