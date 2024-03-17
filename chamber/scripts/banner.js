af5d5c1309157323acb728dfb14c423d64315135
darriola12 <diego.arriolav1@gmail.com>
2024-03-17 05:08:38 -0600

    cambios

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var currentDate = new Date();
     var currentDay = currentDate.getDay(); 
   
-    if (currentDay >= 1 && currentDay <= 3) {
+    if (currentDay >= 0 && currentDay <= 3) {
       var banner = document.getElementById("banner");
       banner.style.display = "block";
       banner.classList.add("fadeIn"); 
