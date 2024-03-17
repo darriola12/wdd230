f18851aef1c32eff12a6fe5d49674360c3944a27
darriola12 <diego.arriolav1@gmail.com>
2024-03-16 23:14:21 -0600

    cav

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,34 @@
+const wather_icon = document.querySelector(".weather-icon");
+const current_temp = document.querySelector(".current_temp");
+const caption  = document.querySelector("figcaption");
+
+// coordinates = 10.297763872080644,  -85.84115989786345
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=10.29&lon=-85.84&appid=1abbefa9b9edd623e602cf9306e0657c&units=imperial&units=imperial";
+
+// call the API
+async function getWeather() {
+
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+            
+        } else {
+            throw Error(await response.text());
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+getWeather();
+
+function displayResults(data) {
+    current_temp.innerHTML = `${data.main.temp.toFixed(0)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    wather_icon.setAttribute('src', iconsrc)
+    wather_icon.setAttribute('alt', desc)
+    caption.textContent = desc;
+}
